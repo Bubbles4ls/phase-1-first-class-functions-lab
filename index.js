91170e8f07e179196d4acf395a2deba8083ac7a4
Surayah Holt <+>
2023-03-24 19:40:02 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+//Declare the variable returnFirstTwoDrivers with 
+//const and assign an anonymous function to it.
+
+const returnFirstTwoDrivers = function anonymousFunction([]) {
+    return  (['Antonia', 'Nuru']);
+}
+
+const returnLastTwoDrivers = function anonymousFunction([]) {
+    return (['Amari', 'Mo']);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers] ;
+
+function createFareMultiplier (anInteger){
+    return function (fare){
+      return fare * anInteger;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers);
+  };
